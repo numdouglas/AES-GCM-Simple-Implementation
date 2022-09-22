b07c927fb46bbde756bbc45480d54cdea495b5bc
numdouglas <salitosmbogz@gmail.com>
2022-09-22 13:06:17 +0300

    Fix misconfigured dependecy classpath

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,11 +7,7 @@ application {
     mainClass.set("m_gcm")
 }
 
-val jar by tasks.getting(Jar::class) {
-    manifest {
-        attributes["Main-Class"] = "m_gcm"
-    }
-}
+
 
 repositories {
     mavenCentral()
@@ -20,6 +16,19 @@ repositories {
 dependencies {
     implementation("org.bouncycastle:bc-fips:1.0.2.3")
     implementation("commons-io:commons-io:2.11.0")
+}
+
+tasks.jar {
+    manifest.attributes["Main-Class"] = "m_gcm"
+
+    archiveBaseName.set("aes_gcm_impl")
+
+    manifest.attributes["Class-Path"] = configurations
+        .runtimeClasspath
+        .get()
+        .joinToString(separator = " ") { file ->
+            "ext_dependencies/${file.name}"
+        }
 }
 
 tasks.withType<JavaExec>() {
